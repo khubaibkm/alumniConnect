03129514020ed530c9e6dc5dcf4752fa8b49df19
WALTER <arish.gzp988@gmail.com>
2023-12-17 22:06:04 +0530

    social links added

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -30,35 +30,37 @@ const Team = () => {
               </span>
               <div className="mt-3">
                 <a
-                  href="https://twitter.com/arish_siddiqui"
+                  href="https://twitter.com/11Arish11"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="mr-4 text-2xl text-primary"
-                  style={{marginRight:"2rem"}}
+                  style={{ marginRight: "2rem" }}
                 >
                   <FaTwitter size={30} />
                 </a>
                 <a
-                  href="https://www.linkedin.com/in/arish-siddiqui/"
+                  href="https://www.linkedin.com/in/arish00/"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="mr-3 text-2xl text-primary"
-                  style={{marginRight:"2rem"}}
+                  style={{ marginRight: "2rem" }}
                 >
                   <FaLinkedin size={30} />
                 </a>
                 <a
-                  href="https://github.com/arish-shah"
+                  href="https://github.com/11WALTER11"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="social-icon text-2xl text-primary"
-                  style={{marginRight:"2rem"}}
+                  style={{ marginRight: "2rem" }}
                 >
                   <FaGithub size={30} />
                 </a>
-              </div><br />
+              </div>
+              <br />
               <p className=" ">
-                Greetings! <br /><br />
+                Greetings! <br />
+                <br />
                 If you have found your way to this page, we extend our sincere
                 congratulations. I am Arish Siddiqui, the visionary behind the
                 inception of AlumniConnect—a platform born out of the idea to
@@ -86,14 +88,16 @@ const Team = () => {
               <h3 className="text-2xl capitalize font-semibold">
                 Khubaib Ahmad
               </h3>
-              <span className="d-block capitalize text-lg text-primary font-light mb-3">Front-end Developer / UI/UX Designer / Coder </span>
+              <span className="d-block capitalize text-lg text-primary font-light mb-3">
+                Front-end Developer / UI/UX Designer / Coder{" "}
+              </span>
               <div className="mt-3">
                 <a
                   href="https://twitter.com/Khubaib56058732"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="mr-4 text-2xl text-primary"
-                  style={{marginRight:"2rem"}}
+                  style={{ marginRight: "2rem" }}
                 >
                   <FaTwitter size={30} />
                 </a>
@@ -102,26 +106,36 @@ const Team = () => {
                   target="_blank"
                   rel="noopener noreferrer"
                   className="mr-3 text-2xl text-primary"
-                  style={{marginRight:"2rem"}}
+                  style={{ marginRight: "2rem" }}
                 >
-                  <FaLinkedin size={30}/>
+                  <FaLinkedin size={30} />
                 </a>
                 <a
                   href="https://github.com/khubaibkm"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="text-2xl text-primary"
-                  style={{marginRight:"2rem"}}
+                  style={{ marginRight: "2rem" }}
                 >
                   <FaGithub size={30} />
                 </a>
-              </div><br />
+              </div>
+              <br />
               <p className=" ">
-                Greetings! <br /><br />
-                Hello! I am Khubaib Ahmad, the passionate mind behind Alumni Connect, a platform dedicated to fostering connections between alumni and students of Integral University. As the architect of this initiative, my vision is to create a vibrant network that provide a bridge for collaborative opportunities. <br /><br />
-                I specialize in crafting visually striking, responsive interfaces that offer delightful user experiences. From wireframes to robust coding, I leverage my skills to turn concepts into reality. Always open to new opportunities and collaborations, I invite alumni, students, and fellow tech enthusiasts to connect. Let's build a future where knowledge knows no bounds. 
-
-
+                Greetings! <br />
+                <br />
+                Hello! I am Khubaib Ahmad, the passionate mind behind Alumni
+                Connect, a platform dedicated to fostering connections between
+                alumni and students of Integral University. As the architect of
+                this initiative, my vision is to create a vibrant network that
+                provide a bridge for collaborative opportunities. <br />
+                <br />I specialize in crafting visually striking, responsive
+                interfaces that offer delightful user experiences. From
+                wireframes to robust coding, I leverage my skills to turn
+                concepts into reality. Always open to new opportunities and
+                collaborations, I invite alumni, students, and fellow tech
+                enthusiasts to connect. Let's build a future where knowledge
+                knows no bounds.
               </p>
             </div>
           </div>
