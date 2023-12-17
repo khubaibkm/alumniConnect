9a1f54d4dc5f967a6ad590e5009e1936b99e41e4
Khubaib <khubaibahmadkm@gmail.com>
2023-12-17 21:11:05 +0530

    team details added

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -5,11 +5,11 @@ const Team = () => {
   return (
     <section className="container p-5 ">
       <h2 className="text-4xl xl:text-5xl text-center text-primary font-bold mt-5">
-        Minds behind AlumniConnect
+        Minds behind Alumni Connect
       </h2>
       <hr className="mx-auto w-12 h-1 bg-success my-4" />
       <p className="text-center text-lg text-gray-800">
-        hope you like our work !
+        We hope you like our work !
       </p>
 
       <div className="row mt-4">
@@ -34,28 +34,31 @@ const Team = () => {
                   target="_blank"
                   rel="noopener noreferrer"
                   className="mr-4 text-2xl text-primary"
+                  style={{marginRight:"2rem"}}
                 >
-                  <FaTwitter />
+                  <FaTwitter size={30} />
                 </a>
                 <a
                   href="https://www.linkedin.com/in/arish-siddiqui/"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="mr-3 text-2xl text-primary"
+                  style={{marginRight:"2rem"}}
                 >
-                  <FaLinkedin />
+                  <FaLinkedin size={30} />
                 </a>
                 <a
                   href="https://github.com/arish-shah"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="text-2xl text-primary"
+                  className="social-icon text-2xl text-primary"
+                  style={{marginRight:"2rem"}}
                 >
-                  <FaGithub />
+                  <FaGithub size={30} />
                 </a>
-              </div>
+              </div><br />
               <p className=" ">
-                Greetings! <br />
+                Greetings! <br /><br />
                 If you have found your way to this page, we extend our sincere
                 congratulations. I am Arish Siddiqui, the visionary behind the
                 inception of AlumniConnect—a platform born out of the idea to
@@ -75,7 +78,7 @@ const Team = () => {
         <div className="col-lg-6">
           <div className="card mb-4">
             <img
-              src="/vite.svg"
+              src="/khubaib.jpeg"
               alt="Hanna Lubin"
               className="card-img-top rounded-3xl"
             />
@@ -83,35 +86,42 @@ const Team = () => {
               <h3 className="text-2xl capitalize font-semibold">
                 Khubaib Ahmad
               </h3>
-              <span className="d-block capitalize text-lg text-primary font-light mb-3"></span>
+              <span className="d-block capitalize text-lg text-primary font-light mb-3">Front-end Developer / UI/UX Designer / Coder </span>
               <div className="mt-3">
                 <a
-                  href="https://twitter.com/arish_siddiqui"
+                  href="https://twitter.com/Khubaib56058732"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="mr-4 text-2xl text-primary"
+                  style={{marginRight:"2rem"}}
                 >
-                  <FaTwitter />
+                  <FaTwitter size={30} />
                 </a>
                 <a
-                  href="https://www.linkedin.com/in/arish-siddiqui/"
+                  href="https://www.linkedin.com/in/khubaibahmad122/"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="mr-3 text-2xl text-primary"
+                  style={{marginRight:"2rem"}}
                 >
-                  <FaLinkedin />
+                  <FaLinkedin size={30}/>
                 </a>
                 <a
-                  href="https://github.com/arish-shah"
+                  href="https://github.com/khubaibkm"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="text-2xl text-primary"
+                  style={{marginRight:"2rem"}}
                 >
-                  <FaGithub />
+                  <FaGithub size={30} />
                 </a>
-              </div>
+              </div><br />
               <p className=" ">
-                Greetings! <br />
+                Greetings! <br /><br />
+                Hello! I am Khubaib Ahmad, the passionate mind behind Alumni Connect, a platform dedicated to fostering connections between alumni and students of Integral University. As the architect of this initiative, my vision is to create a vibrant network that provide a bridge for collaborative opportunities. <br /><br />
+                I specialize in crafting visually striking, responsive interfaces that offer delightful user experiences. From wireframes to robust coding, I leverage my skills to turn concepts into reality. Always open to new opportunities and collaborations, I invite alumni, students, and fellow tech enthusiasts to connect. Let's build a future where knowledge knows no bounds. 
+
+
               </p>
             </div>
           </div>
