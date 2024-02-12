3684a508b2679ccb83a719ac94ded1be996f0840
WALTER <arish.gzp988@gmail.com>
2024-02-12 20:22:17 +0530

    card updated

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -2,64 +2,57 @@ import React from "react";
 
 const Test = () => {
   return (
-    <div className="mt-5 p-5 ">
-      <div class="col-md-6 m-5">
-        <div
-          class="card testimonial-card shadow-sm"
-          style={{ maxWidth: "22rem" }}
-        >
-          <div class="card-up indigo lighten-1"></div>
+    <div className="container mt-5 py-5 w-100">
+      <div className="row justify-content-center">
+        <div className="col-md-6 m-5">
+          <div className="card testimonial-card shadow-sm">
+            <div className="card-up indigo lighten-1"></div>
 
-          <div class="avatar mx-auto my-2 white">
-            <img
-              src="https://mdbootstrap.com/img/Photos/Avatars/img%20%2810%29.webp"
-              class="rounded-circle"
-              alt="woman avatar"
-            />
-          </div>
-
-          <div class="card-body">
-            <div className="d-flex">
-              <h3 class="card-title me-5">Anna Doe</h3>
-
-              <span
-                className="badge badge bg-primary m-1 text-light"
-                style={{
-                  fontSize: "0.8rem",
-                  padding: "1.8%",
-                  height: "fit-content",
-                }}
-              >
-                2024
-              </span>
-              <span
-                className="badge badge bg-primary m-1 text-light"
-                style={{
-                  fontSize: "0.8rem",
-                  padding: "1.8%",
-                  height: "fit-content",
-                }}
-              >
-                CSE
-              </span>
+            <div className="avatar mx-auto my-2 white">
+              <img
+                src="https://mdbootstrap.com/img/Photos/Avatars/img%20%2810%29.webp"
+                className="rounded-circle img-fluid"
+                alt="woman avatar"
+              />
             </div>
 
-            <h6 className="my-0">[jobrole]</h6>
-            <span className="text-muted">[company name]</span>
+            <div className="card-body">
+              <div className="d-flex justify-content-between align-items-center">
+                <h3 className="card-title">Anna Doe</h3>
 
-            <hr />
+                <div className="d-flex">
+                  <span
+                    className="badge bg-primary m-1 text-light"
+                    style={{ fontSize: "0.8rem", whiteSpace: "nowrap" }}
+                  >
+                    2024
+                  </span>
+                  <span
+                    className="badge bg-primary m-1 text-light"
+                    style={{ fontSize: "0.8rem", whiteSpace: "nowrap" }}
+                  >
+                    CSE
+                  </span>
+                </div>
+              </div>
 
-            <p>
-              <i class="fa fa-quote-left"></i> [bio]
-            </p>
-            <div className="d-flex mx-auto p-2 text-xl">
-              <a type="button" class="btn-floating btn-small btn-fb me-2">
-                <i class="fa fa-linkedin"></i>
-              </a>
+              <h6 className="my-0">[jobrole]</h6>
+              <span className="text-muted">[company name]</span>
 
-              <a type="button" class="btn-floating btn-small">
-                <i class="fa fa-envelope"></i>
-              </a>
+              <hr />
+
+              <p>
+                <i className="fa fa-quote-left"></i> [bio]
+              </p>
+
+              <div className="d-flex justify-content-center p-2 text-xl">
+                <a href="#" className="btn-floating btn-small btn-fb me-2">
+                  <i className="fa fa-linkedin"></i>
+                </a>
+                <a href="#" className="btn-floating btn-small">
+                  <i className="fa fa-envelope"></i>
+                </a>
+              </div>
             </div>
           </div>
         </div>
