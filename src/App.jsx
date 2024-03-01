df2a4039eb13c7cd86d49f1ecd6f66b796c5f369
WALTER <arish.gzp988@gmail.com>
2024-03-01 21:26:51 +0530

    loader added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import React, { useEffect, lazy, Suspense } from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import { LineWave } from "react-loader-spinner";
 import "react-toastify/dist/ReactToastify.css";
-import "./App.css";
-import ScrollToTop from "react-scroll-to-top";
 import { Navbar } from "./components/Navbar";
 import Protected from "./components/services/Protected";
 import Footer from "./components/Footer";
-import Error from "./pages/Error"; // Import the Error component
+import Error from "./pages/Error";
 import { Placeholder } from "react-bootstrap";
+
+import "./App.css";
+import ScrollToTop from "react-scroll-to-top";
 
 // Lazy load other components
 const SignUp = lazy(() => import("./pages/SignUp"));
@@ -23,52 +25,59 @@ const UnauthorizedError = lazy(() => import("./pages/UnauthorizedError"));
 const Team = lazy(() => import("./pages/Team"));
 const Test = lazy(() => import("./pages/Test"));
 
-function App() {
+const App = () => {
   return (
-    <>
-      <ScrollToTop
-        className="scrollToTop"
-        smooth
-        style={{ borderRadius: "50%", padding: "20px 42px 40px 16px" }}
-      />
-      <div className="App">
-        <React.Fragment>
-          <ToastContainer />
+    <div className="App">
+      <ToastContainer />
 
-          <Router>
-            <Navbar />
-            <Suspense fallback={<Placeholder />}>
-              <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/signup" element={<SignUp />} />
-                <Route path="/signin" element={<SignIn />} />
-                <Route
-                  path="/On_boarding_form"
-                  element={<Protected Component={OnBoardingForm} />}
-                />
-                <Route path="/profilelist" element={<ProfileList />} />
-                <Route
-                  path="/profile"
-                  element={<Protected Component={MyProfile} />}
-                />
-                <Route path="/undereview" element={<UnderReview />} />
-                <Route path="/admin" element={<AdminDashboard />} />
-                <Route path="/error" element={<UnauthorizedError />} />
-                <Route path="/team" element={<Team />} />
-                <Route path="/test" element={<Test />} />
-
-                {/* Add other routes for your pages */}
-
-                {/* Fallback route for any other paths */}
-                <Route path="*" element={<Error />} />
-              </Routes>
-            </Suspense>
-            <Footer />
-          </Router>
-        </React.Fragment>
-      </div>
-    </>
+      <Router>
+        <Suspense
+          fallback={
+            <LineWave
+              height={300}
+              width={300}
+              radius={5}
+              color="#006aff"
+              ariaLabel="ball-triangle-loading"
+              wrapperStyle={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                minHeight: "100vh",
+              }}
+              wrapperClass=""
+              visible={true}
+            />
+          }
+        >
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route
+              path="/On_boarding_form"
+              element={<Protected Component={OnBoardingForm} />}
+            />
+            <Route path="/profilelist" element={<ProfileList />} />
+            <Route
+              path="/profile"
+              element={<Protected Component={MyProfile} />}
+            />
+            <Route path="/undereview" element={<UnderReview />} />
+            <Route path="/admin" element={<AdminDashboard />} />
+            <Route path="/error" element={<UnauthorizedError />} />
+            <Route path="/team" element={<Team />} />
+            <Route path="/test" element={<Test />} />
+            {/* Add other routes for your pages */}
+            {/* Fallback route for any other paths */}
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </Suspense>
+        <Footer />
+      </Router>
+    </div>
   );
-}
+};
 
 export default App;
