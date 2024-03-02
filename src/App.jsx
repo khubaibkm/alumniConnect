0848323418f684c172ffcb39c4162cfb58fe920c
WALTER <arish.gzp988@gmail.com>
2024-03-03 03:56:13 +0530

    loader centered

--- src/App.jsx
+++ src/App.jsx
@@ -44,6 +44,12 @@ const App = () => {
                 justifyContent: "center",
                 alignItems: "center",
                 minHeight: "100vh",
+                position: "fixed",
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0,
+                background: "rgba(255, 255, 255)",
               }}
               wrapperClass=""
               visible={true}
